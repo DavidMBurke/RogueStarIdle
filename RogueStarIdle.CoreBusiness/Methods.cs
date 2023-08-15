920a9092f274c99dbad63609bb6d487fb15699cd
David Burke <burkedm94@gmail.com>
2023-08-14 21:03:35 -0400

    Dynamically calculate gif lengths

---
+++ RogueStarIdle.CoreBusiness/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.ExceptionServices;
+using System.Text.RegularExpressions;
+
+namespace RogueStarIdle.CoreBusiness
+{
+    public class Methods
+    {
+        public static int GetGifDuration(string localPath)
+        {
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            basePath = Regex.Replace(basePath, @"bin\\Debug\\net6.0", "wwwroot");
+            localPath.TrimStart('/');
+            localPath = Regex.Replace(localPath, "/", "\\");
+            string path = String.Concat(basePath, localPath);
+            Image gif = System.Drawing.Image.FromFile(path);
+            int duration = 0;
+            FrameDimension dimension = new FrameDimension(gif.FrameDimensionsList[0]);
+            int frameCount = gif.GetFrameCount(dimension);
+            for (int i = 0; i < frameCount; i++)
+            {
+                gif.SelectActiveFrame(dimension, i);
+                PropertyItem frameDelay = gif.GetPropertyItem(0x5100);
+                int delay = BitConverter.ToInt32(frameDelay.Value, 0) * 10;
+                duration += delay;
+            }
+            Console.WriteLine(duration.ToString());
+            return duration;
+        }
+    }
+}
